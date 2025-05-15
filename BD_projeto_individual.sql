6f1d3c86d654c7be846e6e4cff29cdd3124aa96b
manuelamiyuki <manuela.matsumoto@sptech.school>
2025-05-15 20:57:08 -0300

    coloquei o banco

---
+++ BD_projeto_individual.sql
@@ -0,0 +1,30 @@
+create database CentralTalks;
+use CentralTalks;
+
+
+create table usuarios(
+id int primary key auto_increment,
+nome varchar (100),
+userName varchar( 100),
+email varchar(50),
+senha varchar (50),
+personagemFavorito varchar(50),
+constraint chk_personagem_favorito check (personagemFavorito in('Chandler','Ross','Monica','Rachel','Joey','Phoebe'))
+);
+
+
+create table quiz(
+id int primary key auto_increment,
+tentativa int default 0,
+qtd_perguntas int,
+qtd_respostas_certas int,
+fkUsuarios int,
+foreign key (fkUsuarios) references usuarios(id)
+);
+
+
+
+
+
+
+drop database CentralTalks;
